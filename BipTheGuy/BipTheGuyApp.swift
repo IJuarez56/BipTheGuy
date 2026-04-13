101ced845fd8ad13e2cb733fbd6e1fde7adc18fd
IJuarez56 <ijuarzez1234@gmail.com>
2026-04-13 12:49:10 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by JUAREZ, ISABELLA on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
